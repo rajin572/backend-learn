daaf13cb3a57022d4c52b697ef9ca2cce57d2dc6
rajin572 <dirrazin572@gmail.com>
2024-09-28 16:35:37 +0600

    Init

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,27 @@
+export type Guardian = {
+  guardianName: string;
+  relationship: string;
+  contactNumber: string;
+  address: string;
+};
+
+export type FullName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type Student = {
+  id: string;
+  fullname: FullName;
+  gender: 'male' | 'female' | 'other';
+  dateOfBirth: string;
+  email: string;
+  contactNumber: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  profileImage: string;
+  isActive: 'active' | 'inactive';
+};
---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose';
+import { FullName, Guardian, Student } from './student.interface';
+
+const fullNameSchema = new Schema<FullName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+const guardianSchema = new Schema<Guardian>({
+  guardianName: { type: String, required: true },
+  relationship: {
+    type: String,
+    enum: ['father', 'mother', 'sister', 'brother', 'other'],
+    required: true,
+  },
+  contactNumber: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentScema = new Schema<Student>({
+  id: { type: String, required: true },
+  fullname: fullNameSchema,
+  gender: { type: String, enum: ['male', 'female', 'other'], required: true },
+  dateOfBirth: { type: String, required: true },
+  email: { type: String, required: true },
+  contactNumber: { type: String, required: true },
+  bloodGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    required: true,
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  guardian: guardianSchema,
+  profileImage: { type: String, required: true },
+});
+
+const StudentModel = model<Student>('Student', studentScema);
+export default StudentModel;
